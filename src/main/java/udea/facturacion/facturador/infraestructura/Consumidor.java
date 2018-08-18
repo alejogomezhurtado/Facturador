f53ef6abc5bc98ae9abe6eaa55052cf8fdcce46a
alejandro gomez hurtado <alejo.gomez.h@gmail.com>
2018-08-18 10:37:43 -0500

    Proyecto final diplomado microservicios nivel 1
    * Se agraga logger

--- src/main/java/udea/facturacion/facturador/infraestructura/Consumidor.java
+++ src/main/java/udea/facturacion/facturador/infraestructura/Consumidor.java
@@ -1,16 +1,19 @@
 package udea.facturacion.facturador.infraestructura;
 
+import com.google.gson.Gson;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.stereotype.Component;
 import udea.facturacion.facturador.controllers.Factura;
+import udea.facturacion.facturador.modelo.DtoFactura;
 
 
 @Component
 public class Consumidor implements MessageListener {
     @Override
     public void onMessage(Message message) {
-        Factura.crear(new String(message.getBody()));
-        System.out.println(new String(message.getBody()));
+        Gson gson = new Gson();
+        DtoFactura factura= gson.fromJson(new String(message.getBody()), DtoFactura.class);
+        Factura.crear(factura);
     }
 }
